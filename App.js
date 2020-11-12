b79554c3bd29c76ea348427b60240498fb2f747f
Trystan Hendricks <dolkhound@gmx.com>
2020-11-11 21:33:07 -0500

    working on header for active screen

--- App.js
+++ App.js
@@ -2,11 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import ActiveScreen from './screens/ActiveScreen';
 import CompletedScreen from './screens/CompletedScreen';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Active"
+        component={ActiveScreen}
+        options={{
+          title: 'Active',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -15,9 +38,10 @@ export default function App() {
         <Tab.Navigator
           initialRouteName="Active"
           tabBarOptions={{
-            activeTintColor: '#1d1075',
+            activeTintColor: '#AA1075',
           }}
         >
+          <Tab.Screen name="Header" component={StackScreen} />
           <Tab.Screen
             name="Active"
             component={ActiveScreen}
