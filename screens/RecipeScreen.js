d0dc4dfcdce9ceee56747c56e23af8c578a4ed86
Trystan Hendricks <dolkhound@gmx.com>
2020-11-12 21:36:35 -0500

    RecipeScreen Frontend

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 function RecipeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>RecipeScreen!</Text>
-      <Text>This will show recipes to make bucha</Text>
+    <View>
+      <Text style={styles.LabelText}>Name</Text>
+      <TextInput style={{ height: 60, fontSize: 28, borderColor: 'red', borderWidth: 2 }} />
+      <Text style={styles.LabelText}>Brewer</Text>
+      <TextInput style={{ height: 60, fontSize: 28, borderColor: 'red', borderWidth: 2 }} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  LabelText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
 
 export default RecipeScreen;
