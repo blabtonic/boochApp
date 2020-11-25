a1c40fd422fdc8ee3309d45aa71543a24c1ff800
Trystan Hendricks <dolkhound@gmx.com>
2020-11-24 21:12:17 -0500

    trying to make this a class removing hooks

--- components/CameraGallery.js
+++ components/CameraGallery.js
@@ -1,65 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import * as Permission from 'expo-permissions';
 import { Camera } from 'expo-camera';
-let camera;
 
-export default function CameraGallery() {
-  const [startCamera, setStartCamera] = useState(false);
+export default class CameraGallery extends Component {
+  camera = null;
 
-  // start function for camera
-  const __startCamera = async () => {
-    const { status } = await Camera.requestPermissionsAsync();
-    if (status === 'granted') {
-      // changes the start state for camera to true
-      setStartCamera(true);
-    } else {
-      Alert.alert('Camera Access was denied');
-    }
+  state = {
+    hasCameraPermission: null,
   };
-  return (
-    <View style={styles.container}>
-      {startCamera ? (
-        <Camera
-          style={{ flex: 1, width: '100%' }}
-          ref={(res) => {
-            camera = res;
-          }}
-        />
-      ) : (
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <TouchableOpacity
-            onPress={__startCamera}
-            style={{
-              width: 130,
-              borderRadius: 5,
-              backgroundColor: '#8F00AD',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 40,
-            }}
-          >
-            <Text
-              style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center',
-              }}
-            >
-              TAKE PHOTO
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </View>
-  );
+  render() {
+    const { hasCameraPermission } = this.state;
+    return (
+      <View>
+        <Camera ref={(camera) => (this.camera = camera)} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
