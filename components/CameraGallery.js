2dd13ffa83732e8df8362cbe0ab486911dd4cead
Trystan Hendricks <dolkhound@gmx.com>
2020-11-25 17:28:35 -0500

    camera added to screen maybe create a component?

--- components/CameraGallery.js
+++ components/CameraGallery.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
@@ -27,17 +27,27 @@ export default class CameraGallery extends Component {
     }
     return (
       <View>
-        <Camera ref={(camera) => (this.camera = camera)} />
+        <Camera style={styles.preview} ref={(camera) => (this.camera = camera)} />
       </View>
     );
   }
 }
 
+const { width: winWidth, height: winHeight } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  preview: {
+    height: winHeight,
+    width: winWidth,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
   },
 });
